8dac971879b7c67261a0e07f2bdc210393e639c6
Yingyao Lu <emilylu123@gmail.com>
2021-08-06 17:25:00 +0930

    Rxjs obserable and subscribe

--- todo/src/app/todos/todos.service.ts
+++ todo/src/app/todos/todos.service.ts
@@ -1,12 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodosService {
+  listChanged = new Subject<string[]>();
   todoList: string[] = ['Study', 'Eat', 'Sleep'];
 
   addNewTodoItem(item: any) {
     this.todoList.push(item);
+    this.listChanged.next(this.todoList);
+  }
+
+  removeTodoItem(content: string) {
+    this.todoList = this.todoList.filter((todo) => {
+      return todo !== content;
+    });
+    console.log(this.todoList);
+    this.listChanged.next(this.todoList);
   }
 }
