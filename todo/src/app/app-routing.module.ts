f90d787442e0ca62d4373e3d98be863fc356ba47
Yingyao Lu <emilylu123@gmail.com>
2021-08-06 15:57:48 +0930

    Passing data around component and event emit

--- todo/src/app/app-routing.module.ts
+++ todo/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InputItemComponent } from './input-item/input-item.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TodosComponent },
+  { path: 'input', component: InputItemComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
